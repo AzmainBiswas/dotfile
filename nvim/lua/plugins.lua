fe091e091fd9907f7bb4c173ede5a2cc23dc8f0f
AzmainBiswas <azmainbiswas007@gmail.com>
2022-10-08 23:25:49 +0530

    adding to github

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,73 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require ('packer').startup(function(use)
+    -- packer can manage itself
+    use 'wbthomason/packer.nvim'
+    
+    use {'lewis6991/impatient.nvim',
+        config = function() require('impatient') end 
+    }
+    
+    -- finder in vim
+    use {'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                         , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    
+    --transparent
+    use 'xiyaowong/nvim-transparent'
+    
+    -- file tree in vim
+    use {'kyazdani42/nvim-tree.lua',
+    requires = {
+        'kyazdani42/nvim-web-devicons', -- optional, for file icons
+        },
+        tag = 'nightly' -- optional, updated every week. (see issue #1193)
+    }
+    
+    -- hexcolor indecator
+    use({
+        'norcalli/nvim-colorizer.lua',
+        event = 'CursorHold',
+        config = function()
+                require('colorizer').setup()
+        end,
+    })
+    
+    -- status bar in nvim
+    use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+    
+    -- indent blankline
+    use 'lukas-reineke/indent-blankline.nvim'
+    -- bufferline--
+    use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
+    --beter syntex highlight --
+    use "nvim-treesitter/nvim-treesitter"
+
+    --bracket auto complete
+    --use 'rstacruz/vim-closer'
+    
+    -- compliction
+    use {'neoclide/coc.nvim', branch = 'release'}
+    use {
+	    "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }    
+    use 'rafcamlet/coc-nvim-lua'
+    
+    --spell check
+    use 'inkarkat/vim-SpellCheck'
+    use 'inkarkat/vim-ingo-library'
+
+    
+    --tatex
+    use 'lervag/vimtex'
+    use 'SirVer/ultisnips'
+    
+    -- colorscheme
+    use {'dracula/vim', as = 'dracula'}
+    use "EdenEast/nightfox.nvim"
+    end)
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,62 @@
+-- Settings
+local o = vim.o
+local g = vim.g
+
+-- Decrease update time
+o.timeoutlen = 500
+o.updatetime = 200
+
+-- Number of screen lines to keep above and below the cursor
+o.scrolloff = 8
+
+-- Better editor UI
+o.number = true
+o.numberwidth = 5
+o.relativenumber = true
+o.cursorline = true
+o.signcolumn = "number"
+o.colorcolumn = "99999" -- fix columns
+
+-- Better editing experience
+o.expandtab = true
+o.smarttab = true
+o.cindent = true
+-- o.autoindent = true
+o.wrap = true
+o.textwidth = 300
+o.tabstop = 4
+o.shiftwidth = 0
+o.softtabstop = -1 -- If negative, shiftwidth value is used
+o.list = true
+o.listchars = 'trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂'
+
+-- Makes neovim and host OS clipboard play nicely with each other
+o.clipboard = 'unnamedplus'
+
+-- Case insensitive searching UNLESS /C or capital in search
+o.ignorecase = true
+o.smartcase = true
+
+-- Undo and backup options
+o.backup = false
+o.writebackup = false
+o.undofile = true
+o.swapfile = false
+
+-- Remember 50 items in commandline history
+o.history = 50
+
+-- Better buffer splitting
+o.splitright = true
+o.splitbelow = true
+
+-- Preserve view while jumping
+o.jumpoptions = 'view'
+
+-- spell checking
+vim.o.spell = true
+vim.o.spelllang = "en_us"
+--nodejs provider of
+vim.g.loaded_node_provider = 0
+--purn suport desable
+vim.g.loaded_purl_provider = 0
